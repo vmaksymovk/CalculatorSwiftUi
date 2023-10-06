d3da280d0aac9abc01bdffda168ac1b97e66c14c
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-10-06 21:02:17 +0200

    Created project/ connect for GitHub

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Влад on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
